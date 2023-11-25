6bd5fc2eeb09e6c5a8a6a128309f14023645cf55
Gilang <111492686+Gipang07@users.noreply.github.com>
2023-11-25 23:31:45 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Apa kepanjangan dari HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Apa pengertian CSS?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "CSS digunakan untuk mengatur ?",
+    answer: "Presentasi tata letak dan tampilan",
+    options: [
+      "Bahasa pemrograman",
+      "HLogika program",
+      "Presentasi tata letak dan tampilan",
+      "Koneksi database"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Apa fungsi dari JavaScript?",
+    answer: "Memberikan interaktivitas pada halaman web",
+    options: [
+      "Menambahkan style ke elemen HTML",
+      "Memberikan interaktivitas pada halaman web",
+      "Menambahkan style ke elemen HTML",
+      "Mengelola database"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Tag HTML yang digunakan untuk membuat hyperlink adalah:",
+    answer: "a",
+    options: [
+      "url",
+      "link",
+      "href",
+      "a"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Apa yang dimaksud dengan box model dalam CSS?",
+    answer: "Model untuk mendefinisikan border, margin, dan padding",
+    options: [
+      "Model untuk mendefinisikan border, margin, dan padding",
+      "Model untuk menggambar kotak di HTML",
+      "Model untuk membuat input box",
+      ""
+    ]
+  },
+  {
+    numb: 7,
+    question: "Selector CSS #header merujuk pada:",
+    answer: "Elemen dengan id header",
+    options: [
+      "Semua elemen dengan nama header",
+      "Elemen dengan id header",
+      "Semua elemen header di halaman",
+      "Semua elemen dengan class header"
+    ]
+  },
+  {
+    numb: 8,
+    question: "CSS Flexbox digunakan untuk:",
+    answer: "Tata letak responsif ",
+    options: [
+      "Tata letak responsif ",
+      "Animasi",
+      "Membuat gambar responsif",
+      "Menambahkan warna ke elemen HTML"
+    ]
+  },
+  {
+    numb: 9,
+    question: "CSS Grid digunakan untuk:",
+    answer: "Tata letak halaman ",
+    options: [
+      "Menyembunyikan elemen dari halaman",
+      "Menambahkan efek transisi",
+      "Animasi",
+      "Tata letak halaman"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Apa perbedaan antara let, const, dan var dalam JavaScript?",
+    answer: "let dan const bersifat blok-scope, sementara var bersifat function-scope",
+    options: [
+      "var hanya dapat digunakan untuk deklarasi variabel yang dapat diubah",
+      "Semua memiliki fungsi yang sama",
+      "let dan const bersifat blok-scope, sementara var bersifat function-scope ",
+      "const hanya dapat digunakan untuk deklarasi variabel yang tidak dapat diubah"
+    ]
+  },
+];
